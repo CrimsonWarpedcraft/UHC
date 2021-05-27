29aa1938dacc0185bdc858d70b1535bff51a65a1
Levi Muniz <levi.muniz17@gmail.com>
2021-05-26 18:05:26 -0600

    Initial skeleton for registering event listeners

--- src/main/java/com/crimsonwarpedcraft/uhc/Uhc.java
+++ src/main/java/com/crimsonwarpedcraft/uhc/Uhc.java
@@ -1,11 +1,12 @@
 package com.crimsonwarpedcraft.uhc;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.papermc.lib.PaperLib;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 /**
- * Created by Levi Muniz on 7/29/20.
+ * Entry point for UHC plugin.
  *
  * @author Copyright (c) Levi Muniz. All Rights Reserved.
  */
@@ -15,6 +16,17 @@ public class Uhc extends JavaPlugin {
   public void onEnable() {
     PaperLib.suggestPaper(this);
 
+    // Register event listeners
+    registerListeners();
+
     saveDefaultConfig();
+  }
+
+  /** Helper function for registering event listeners. */
+  @SuppressFBWarnings("DLS_DEAD_LOCAL_STORE") // TODO remove once we add something here
+  private void registerListeners() {
+    // Plugin manager used for registering event listeners with
+    PluginManager manager =  this.getServer().getPluginManager();
+
   }
 }
